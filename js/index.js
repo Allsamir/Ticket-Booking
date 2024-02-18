c050ade3d4bb6d37c27033b708a33e110940ab88
Al Samir <shahinur1591@gmail.com>
2024-02-18 21:01:21 +0600

    Half project done

---
+++ js/index.js
@@ -0,0 +1,19 @@
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            navButtonBorderColor: 'rgba(29, 209, 0, 0.40)',
+            navButtonBg: 'rgba(29, 209, 0, 0.10)',
+            green: '#1DD100',
+            couponBg1: '#FFBF0F',
+            couponHeading: 'rgba(3, 7, 18, 0.80)',
+            couponP: 'rgba(3, 7, 18, 0.50)',
+            couponBg2: '#F78C9C',
+            ticketBg: '#F7F8F8',
+            destinationBg: '#F7F8F8',
+            locationBg: 'rgba(3, 7, 18, 0.05)',
+            seatBg: 'rgba(29, 209, 0, 0.15)',
+          }
+        }
+      }
+    }
