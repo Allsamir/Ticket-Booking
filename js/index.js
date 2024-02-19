6cb4d29817ed526edd7ff0adf3e087ac1f30dd70
Al Samir <shahinur1591@gmail.com>
2024-02-19 23:18:57 +0600

    Project almost done

--- js/index.js
+++ js/index.js
@@ -16,6 +16,7 @@
             available:'rgba(3, 7, 18, 0.50)',
             driverSeatBg: 'rgba(3, 7, 18, 0.10)',
             seatBg: '#F7F8F8',
+            success: '#27AE60'
           }
         }
       }
@@ -38,12 +39,11 @@
     const couponCodeApply = document.getElementById('coupon-code-apply');
 
     //
-    const showSelectedSeatSection = document.querySelector('.show-seat');
+    const showSelectedSeatSection = document.getElementById('show-seat');
     const calculationSection = document.querySelector('.calculation')
 
     for (const singleSeat of mainSeat) {
       $(singleSeat).click((e) => {
-        
         console.log("Hello", e.target.innerText, limitSeat);
         const seat = e.target;
         const selectedSeatID = e.target.innerText;
@@ -63,19 +63,7 @@
             seatCount.classList.add('bg-green');
             seatCount.innerText = selectedSeat;
             // creating span and appending it into calculationSection
-            const newDiv = document.createElement('div');
-            const span1 = document.createElement('span');
-            const span2 = document.createElement('span');
-            const span3 = document.createElement('span');
-            newDiv.classList.add('flex', 'justify-between', 'w-full', 'pb-4');
-            span1.classList.add('text-base', 'text-black', 'font-normal');
-            span2.classList.add('text-base', 'text-black', 'font-normal');
-            span3.classList.add('text-base', 'text-black', 'font-normal');
-            span1.innerText = selectedSeatID;
-            span2.innerText = "Economic";
-            span3.innerText = "550 BDT";
-            newDiv.append(span1, span2, span3);
-            showSelectedSeatSection.appendChild(newDiv);
+            showSeats(selectedSeatID)
             // update total
             ticketPrice = ticketPrice + 550;
             total.innerText = ticketPrice;
@@ -84,7 +72,59 @@
             grandTotal.innerText = grandTotalCount;
 
             // coupon code section logic
-                    if (limitSeat >= 4) {
+            checkCoupon()
+            handleClick(e.target);
+        } 
+      })
+    }
+
+
+    //// On submit function
+    function handleSubmit (e) {
+      e.preventDefault();
+      if (limitSeat > 0) {
+        document.getElementById('my_modal_1').showModal();
+        resetEveryThing()
+      }
+    }
+
+    function handleClick(clickedButton) {
+    clickedButton.disabled = true;
+  }
+    
+    function resetEveryThing() {
+      remainingSeat = 40;
+      limitSeat = 0;
+      selectedSeat = 0;
+      ticketPrice = 0;
+      grandTotalCount = 0;
+      seatLeft.innerText = remainingSeat;
+      seatCount.classList.remove('bg-green');
+      seatCount.innerText = selectedSeat;
+      total.innerText = ticketPrice;
+      grandTotal.innerText = grandTotalCount;
+      formSubmit.classList.remove("bg-green");
+      formSubmit.setAttribute("disabled", "true");
+      while (showSelectedSeatSection.firstChild) {
+        showSelectedSeatSection.removeChild(showSelectedSeatSection.firstChild)
+      };
+      emptyInput('name');
+      emptyInput('number');
+      emptyInput('email');
+      for (const singleSeat of mainSeat) {
+        singleSeat.disabled = false;
+        singleSeat.classList.remove("bg-green", "text-white");
+        singleSeat.classList.add("bg-seatBg");
+      }
+    }
+
+    function emptyInput (id) {
+      const input = document.getElementById(id);
+      input.value = '';
+    }
+
+    function checkCoupon() {
+      if (limitSeat >= 4) {
                       // checking coupon code and update the value of grand total
                       const spanAlert = document.createElement('span');
                       spanAlert.classList.add('text-red-500', 'pr-4', 'pt-4', 'invalid-coupon');
@@ -93,7 +133,7 @@
                       coupon.classList.remove('hidden');
                       $(couponCodeApply).click(() => {
                         console.log(couponCode.value);
-                        console.log(grandTotalCount);
+                        console.log(grandTotalCount); //
                         if (couponCode.value === 'NEW15'){
                           let discountAmount = grandTotalCount * 0.15;
                           grandTotal.innerText = grandTotalCount - discountAmount;
@@ -109,11 +149,20 @@
                         }
                       })
                     }
-        } 
-      })
     }
 
-
-    //// On submit function
-
-    
+    function showSeats (seatID) {
+            const newDiv = document.createElement('div');
+            const span1 = document.createElement('span');
+            const span2 = document.createElement('span');
+            const span3 = document.createElement('span');
+            newDiv.classList.add('flex', 'justify-between', 'w-full', 'pb-4');
+            span1.classList.add('text-base', 'text-black', 'font-normal');
+            span2.classList.add('text-base', 'text-black', 'font-normal');
+            span3.classList.add('text-base', 'text-black', 'font-normal');
+            span1.innerText = seatID;
+            span2.innerText = "Economic";
+            span3.innerText = "550 BDT";
+            newDiv.append(span1, span2, span3);
+            showSelectedSeatSection.appendChild(newDiv);
+    }
