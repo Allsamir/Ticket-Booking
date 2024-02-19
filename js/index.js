b78e5a92605247ee4b3a4d21cda5c5cf0fbea170
Al Samir <shahinur1591@gmail.com>
2024-02-19 15:21:15 +0600

    On page functionality done

--- js/index.js
+++ js/index.js
@@ -38,15 +38,15 @@
     const couponCodeApply = document.getElementById('coupon-code-apply');
 
     //
+    const showSelectedSeatSection = document.querySelector('.show-seat');
     const calculationSection = document.querySelector('.calculation')
-    console.log(calculationSection);
 
     for (const singleSeat of mainSeat) {
       $(singleSeat).click((e) => {
         
         console.log("Hello", e.target.innerText, limitSeat);
         const seat = e.target;
-        
+        const selectedSeatID = e.target.innerText;
         if (limitSeat <= 3) {
             limitSeat++;
             remainingSeat--;
@@ -63,7 +63,19 @@
             seatCount.classList.add('bg-green');
             seatCount.innerText = selectedSeat;
             // creating span and appending it into calculationSection
-            
+            const newDiv = document.createElement('div');
+            const span1 = document.createElement('span');
+            const span2 = document.createElement('span');
+            const span3 = document.createElement('span');
+            newDiv.classList.add('flex', 'justify-between', 'w-full', 'pb-4');
+            span1.classList.add('text-base', 'text-black', 'font-normal');
+            span2.classList.add('text-base', 'text-black', 'font-normal');
+            span3.classList.add('text-base', 'text-black', 'font-normal');
+            span1.innerText = selectedSeatID;
+            span2.innerText = "Economic";
+            span3.innerText = "550 BDT";
+            newDiv.append(span1, span2, span3);
+            showSelectedSeatSection.appendChild(newDiv);
             // update total
             ticketPrice = ticketPrice + 550;
             total.innerText = ticketPrice;
@@ -74,6 +86,10 @@
             // coupon code section logic
                     if (limitSeat >= 4) {
                       // checking coupon code and update the value of grand total
+                      const spanAlert = document.createElement('span');
+                      spanAlert.classList.add('text-red-500', 'pr-4', 'pt-4', 'invalid-coupon');
+                      calculationSection.appendChild(spanAlert);
+                      const wrongCouponAlert = document.querySelector('.invalid-coupon');
                       coupon.classList.remove('hidden');
                       $(couponCodeApply).click(() => {
                         console.log(couponCode.value);
@@ -81,12 +97,23 @@
                         if (couponCode.value === 'NEW15'){
                           let discountAmount = grandTotalCount * 0.15;
                           grandTotal.innerText = grandTotalCount - discountAmount;
+                          calculationSection.removeChild(wrongCouponAlert)
+                          coupon.classList.add('hidden');
                         } else if (couponCode.value === 'Couple 20') {
                           let discountAmount = grandTotalCount * 0.20;
                           grandTotal.innerText = grandTotalCount - discountAmount;
+                          calculationSection.removeChild(wrongCouponAlert)
+                          coupon.classList.add('hidden');
+                        } else {                        
+                          spanAlert.innerText = "Invalid Coupon";
                         }
                       })
                     }
         } 
       })
     }
+
+
+    //// On submit function
+
+    
