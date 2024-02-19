f3e15ac1817450c49952652e58cbfc48597d5cd0
Al Samir <shahinur1591@gmail.com>
2024-02-19 12:27:32 +0600

    Logic done almost

--- js/index.js
+++ js/index.js
@@ -23,12 +23,70 @@
 
     const mainSeat = document.getElementsByClassName('main-seat');
     let remainingSeat = 40; // -1
-    const ticketPrice = 550; // + 550;
+    let ticketPrice = 0; // + 550;
+    let grandTotalCount = 0;
     let limitSeat = 0; // +1
     let selectedSeat = 0; // +1
 
+    const seatLeft = document.getElementById('seat-left');
+    const formSubmit = document.getElementById('form-submit');
+    const seatCount = document.getElementById('seat-count');
+    const total = document.getElementById('total');
+    const grandTotal = document.getElementById('grand-total');
+    const coupon = document.getElementById('coupon');
+    const couponCode = document.getElementById('coupon-code');
+    const couponCodeApply = document.getElementById('coupon-code-apply');
+
+    //
+    const calculationSection = document.querySelector('.calculation')
+    console.log(calculationSection);
+
     for (const singleSeat of mainSeat) {
       $(singleSeat).click((e) => {
-        console.log("Hello", e.target);
+        
+        console.log("Hello", e.target.innerText, limitSeat);
+        const seat = e.target;
+        
+        if (limitSeat <= 3) {
+            limitSeat++;
+            remainingSeat--;
+            selectedSeat++;
+            // selected seat color change
+            seat.classList.add("bg-green", "text-white");
+            seat.classList.remove("bg-seatBg");
+            // changing the set-left
+            seatLeft.innerText = remainingSeat;
+            // form-submit button
+            formSubmit.classList.add("bg-green");
+            formSubmit.removeAttribute("disabled");
+            // seat count ++
+            seatCount.classList.add('bg-green');
+            seatCount.innerText = selectedSeat;
+            // creating span and appending it into calculationSection
+            
+            // update total
+            ticketPrice = ticketPrice + 550;
+            total.innerText = ticketPrice;
+            // udate grandTotal
+            grandTotalCount = grandTotalCount + 550;
+            grandTotal.innerText = grandTotalCount;
+
+            // coupon code section logic
+                    if (limitSeat >= 4) {
+                      // checking coupon code and update the value of grand total
+                      coupon.classList.remove('hidden');
+                      $(couponCodeApply).click(() => {
+                        console.log(couponCode.value);
+                        console.log(grandTotalCount);
+                        if (couponCode.value === 'NEW15'){
+                          let discountAmount = grandTotalCount * 0.15;
+                          grandTotal.innerText = grandTotalCount - discountAmount;
+                        } else if (couponCode.value === 'Couple 20') {
+                          let discountAmount = grandTotalCount * 0.20;
+                          grandTotal.innerText = grandTotalCount - discountAmount;
+                        }
+                      })
+                    }
+        } 
       })
     }
